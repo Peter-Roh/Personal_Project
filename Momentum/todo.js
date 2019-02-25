79b03d736a47ddd5a316242404929c1adff96c06
Peter Roh <minchul.roh.peter@gmail.com>
2019-02-25 18:07:08 +0900

    2019.02.25

--- Momentum/todo.js
+++ Momentum/todo.js
@@ -2,38 +2,57 @@ const toDoForm = document.querySelector(".js-todoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS = 'toDos'
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function paintToDo(text) {
-    const li = document.createElement("li");
-    const delBtn = document.createElement("button");
-    delBtn.innerText = "❌"
-    const span = document.createElement("span");
-    span.innerText = text;
-    li.appendChild(span);
-    li.appendChild(delBtn);
-    toDoList.appendChild(li);
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  const span = document.createElement("span");
+  const newID = toDos.length + 1;
+
+  delBtn.innerText = "❌";
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  li.id = newID;
+  toDoList.appendChild(li);
+
+  const toDoObj = {
+    text: text,
+    id: newID
+  };
+
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    const currentValue = toDoInput.value;
-    paintToDo(currentValue);
-    toDoInput.value = "";
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
 }
 
 function loadToDo() {
-    const toDos = localStorage.getItem(TODOS_LS);
+  const loadedToDos = localStorage.getItem(TODOS_LS);
 
-    if(toDos !== null)
-    {
-
-    }
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(function(toDO) {
+      paintToDo(toDo.text);
+    });
+  }
 }
 
 function init() {
-    loadToDo();
-    toDoForm.addEventListener("submit", handleSubmit);
+  loadToDo();
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
