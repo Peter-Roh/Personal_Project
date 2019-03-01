602feb127c4bed78b419aac1c967349c11bcd5e9
Peter Roh <minchul.roh.peter@gmail.com>
2019-03-01 14:01:26 +0900

    2019.03.01

--- Momentum/weather.js
+++ Momentum/weather.js
@@ -1,5 +1,18 @@
+const weather = document.querySelector(".js-weather");
 const API_KEY = "2f54ae9c0a56d5f57b5f55e11fce7594"
 const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json) {
+    const temperature = json.main.temp;
+    const place = json.sys.country;
+    weather.innerText = `${temperature}°C at ${place}`;
+  });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -14,6 +27,7 @@ function handleGeoSuccess(position) {
   };
 
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -30,6 +44,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
   }
 }
 
