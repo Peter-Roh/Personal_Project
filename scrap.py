accfffca31627f325acc4a7ec5a314f30ee142f2
Peter-Roh <rmc2@naver.com>
2020-04-17 14:45:29 +0900

    stackoverflow scrapper

--- scrap.py
+++ scrap.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 import csv
 
 URL = "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50"
+URL_2 = "https://stackoverflow.com/jobs?q=python&sort=i"
+
+# indeed
 
 def extract_pages():
     result = requests.get(URL)
@@ -16,21 +19,26 @@ def extract_pages():
     return max_page
 
 def extract_job(html):
-    title = html.find("div", {"class": "title"}).find("a")["title"]
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
     company = html.find("span", {"class": "company"})
     if company.find("a") is not None:
         company = str(company.find("a").string)
     else:
         company = str(company.string)
     company = company.strip()
-    location = html.find("span", {"class": "location"}).string
+    location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
     job_id = html["data-jk"]
-    return {"title": title, "company": company, "location": location, "link": f"https://kr.indeed.com/jobs?q=python&limit=50&start=200&vjk={job_id}"}
+    return {
+    "title": title, 
+    "company": company, 
+    "location": location, 
+    "link": f"https://kr.indeed.com/jobs?q=python&limit=50&start=200&vjk={job_id}"
+    }
 
 def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        print(f"Scrapping page {page + 1}...")
+        print(f"Scrapping Indeed page {page + 1}...")
         result = requests.get(URL+"&start={}".format(page*50))
         soup = BeautifulSoup(result.text, 'html.parser')
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
@@ -39,14 +47,59 @@ def extract_jobs(last_page):
             jobs.append(job)
     return jobs
 
+# stackoverflow
+
+def get_last_page():
+    result = requests.get(URL_2)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+def extract_so_job(html):
+    title = html.find("h2").find("a")["title"]
+    company_row = html.find("h3").find_all("span")
+    company = company_row[0].get_text(strip=True)
+    if len(company_row) == 2:
+        location = company_row[1].get_text(strip=True).strip("-").strip(" \r").strip("\n")
+    else:
+        location = ""
+    job_id = html['data-jobid']
+    return {
+    'title': title, 
+    'company': company, 
+    'location': location, 
+    'apply_link': f"https://stackoverflow.com/jobs/{job_id}"
+    }
+
+def extract_so_jobs(last_page):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scrapping Stackoverflow page {page + 1}...")
+        result = requests.get(f"{URL_2}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        for result in results:
+            job = extract_so_job(result)
+            jobs.append(job)
+    return jobs
+
+# saving
+
 def save_to_file(jobs):
     file = open("jobs.csv", mode="w")
     writer = csv.writer(file)
     writer.writerow(["title", "company", "location", "link"])
     for job in jobs:
         writer.writerow(job.values())
+    return
 
+def get_jobs():
+    last_page = extract_pages()
+    indeed_jobs = extract_jobs(last_page)
+    last_page = get_last_page()
+    stack_jobs = extract_so_jobs(last_page)
+    jobs = indeed_jobs + stack_jobs
+    return jobs
 
-last_page = extract_pages()
-jobs = extract_jobs(last_page)
-save_to_file(jobs)
+save_to_file(get_jobs())
